2bbbc80170f0761078155e20ddfa4f896e344b6c
Marcos Manoel <marcosmss1304@gmail.com>
2019-06-20 22:54:38 -0300

    first commit

---
+++ form.php
@@ -0,0 +1,21 @@
+<?php
+  $emailEnviar = "marcosmss1304@gmail.com";
+  $nome = $_POST['nome'];
+  $telefone = $_POST['telefone'];
+  $email = $_POST['email'];
+  $mensagem = $_POST['mensagem'];
+
+  if($_POST['enviar']) {
+    mail("$emailEnviar", "$mensagem", "
+    
+    Este é um contato enviado pelo site por $nome com as seguintes informações:
+
+    Nome: "$nome"
+    Telefone: "$telefone"
+    Email: "$email"
+    --
+    Mensagem: "$mensagem"
+
+    ";)
+  }
+?>
